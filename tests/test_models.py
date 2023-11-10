f07a7aacfc287a4ec468e5066877d075110be033
Clair Mould <86794332+clmould@users.noreply.github.com>
2023-11-10 15:33:25 +0000

    Add parameterisation mean, min, max test cases.

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,31 +1,62 @@
 """Tests for statistics functions within the Model layer."""
 
+import os
 import numpy as np
 import numpy.testing as npt
-import os
 import pytest
+from inflammation.models import daily_mean
+from inflammation.models import daily_max
+from inflammation.models import daily_min
 
 
-def test_daily_mean_zeros():
-    """Test that mean function works for an array of zeros."""
-    from inflammation.models import daily_mean
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2], [3, 4], [5, 6]], [3, 4]),
+    ],
+)
+def test_daily_mean(test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
 
-    test_input = np.array([[0, 0], [0, 0], [0, 0]])
-    test_result = np.array([0, 0])
+    npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
+
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[3, 4], [5, 6], [7, 8]], [7, 8]),
+        ([[4, -2, 5], [1, -6, 2], [-4, -1, 9]], [4, -1, 9]),
+    ],
+)
+def test_daily_max(test, expected):
+    """Test that the max function works for an array of zeros and positive integers."""
 
     # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_mean(test_input), test_result)
+    npt.assert_array_equal(daily_max(np.array(test)), np.array(expected))
 
 
-def test_daily_mean_integers():
-    """Test that mean function works for an array of positive integers."""
-    from inflammation.models import daily_mean
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[5, 6], [7, 8], [9, 10]], [5, 6]),
+        ([[4, -2, 5], [1, -6, 2], [-4, -1, 9]], [-4, -6, 2]),
+    ],
+)
+def test_daily_min(test, expected):
+    """Test that max function works for an array of zeros and positive integers."""
 
-    test_input = np.array([[1, 2], [3, 4], [5, 6]])
-    test_result = np.array([3, 4])
+    npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
 
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_mean(test_input), test_result)
+
+def test_daily_min_string():
+    """Test for TypeError when passing strings"""
+    from inflammation.models import daily_min
+
+    with pytest.raises(TypeError):
+        error_expected = daily_min([["Hello", "there"], ["General", "Kenobi"]])
 
 
 def test_load_from_json(tmpdir):
@@ -36,42 +67,3 @@ def test_load_from_json(tmpdir):
         temp_json_file.write('[{"observations":[1, 2, 3]},{"observations":[4, 5, 6]}]')
     result = load_json(example_path)
     npt.assert_array_equal(result, [[1, 2, 3], [4, 5, 6]])
-
-
-def test_daily_max_zeros():
-    """Test that the max function works for an array of zeros."""
-    from inflammation.models import daily_max
-
-    test_input = np.array([[0, 0], [0, 0], [0, 0]])
-    test_result = np.array([0, 0])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_max(test_input), test_result)
-
-
-def test_daily_max_integers():
-    """Test that max function works for an array of positive integers."""
-    from inflammation.models import daily_max
-
-    test_input = np.array([[1, 2], [3, 4], [5, 6]])
-    test_result = np.array([5, 6])
-
-    npt.assert_array_equal(daily_max(test_input), test_result)
-
-
-def test_daily_min_integers():
-    """Test that max function works for an array of positive integers."""
-    from inflammation.models import daily_min
-
-    test_input = np.array([[1, 2], [3, 4], [5, 6]])
-    test_result = np.array([1, 2])
-
-    npt.assert_array_equal(daily_min(test_input), test_result)
-
-
-def test_daily_min_string():
-    """Test for TypeError when passing strings"""
-    from inflammation.models import daily_min
-
-    with pytest.raises(TypeError):
-        error_expected = daily_min([["Hello", "there"], ["General", "Kenobi"]])
